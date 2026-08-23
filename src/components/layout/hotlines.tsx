474708bcb39b5ad4a2202052d7211b8f394445dc
RyannKim327 <rksesgundo123@gmail.com>
2026-08-24 02:19:19 +0800

    temporarily removed marquee

--- src/components/layout/hotlines.tsx
+++ src/components/layout/hotlines.tsx
@@ -3,21 +3,21 @@ import { hotlines } from "@/lib/data/hotlines";
 export default function Hotlines() {
   return (
     <div className="flex justify-center text-sm w-full bg-red-600 text-white">
-      <marquee behavior="" direction="">
-        <div className="flex gap-5 font-semibold p-2">
-          {
-            hotlines.map((hotline, i: number) => {
-              if (hotline.head) {
-                return (
-                  <span key={`${i}. ${hotline.name}`}>
-                    {hotline.name} {hotline.dial.join(" | ")}
-                  </span>
-                )
-              }
-            })
-          }
-        </div>
-      </marquee>
+      {/* <marquee behavior="" direction=""> */}
+      <div className="flex gap-5 font-semibold p-2">
+        {
+          hotlines.map((hotline, i: number) => {
+            if (hotline.head) {
+              return (
+                <span key={`${i}. ${hotline.name}`}>
+                  {hotline.name} {hotline.dial.join(" | ")}
+                </span>
+              )
+            }
+          })
+        }
+      </div>
+      {/* </marquee> */}
     </div>
   )
 }
