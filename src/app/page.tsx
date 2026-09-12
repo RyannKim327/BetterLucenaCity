a700d384590b58a2f24e3ab58ea9febc4594ff95
RyannKim327 <rksesgundo123@gmail.com>
2026-09-12 22:37:22 +0800

    tests and seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import AnnouncementSection from "@/components/sections/announcement";
 import ServicesSection from "@/components/sections/services";
 import AboutSection from "@/components/sections/about";
 import HeroSection from "@/components/sections/hero";
-import Contributors from "@/components/sections/contributors";
 import History from "@/components/sections/history";
 
 
